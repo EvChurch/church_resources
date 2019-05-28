c0c34ac6f15716757bd8b6a5785ca2f152584966
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-05-28 23:09:47 +1200

    update og description

--- app/decorators/resource_decorator.rb
+++ app/decorators/resource_decorator.rb
@@ -2,6 +2,7 @@
 
 class ResourceDecorator < ApplicationDecorator
   decorates_association :connection_scriptures
+  decorates_association :authors
 
   def action
     'view'
@@ -16,10 +17,21 @@ class ResourceDecorator < ApplicationDecorator
   end
 
   def related
-    resources = Resource.published.order('RANDOM()').limit(3).left_outer_joins(:series, :authors, :topics).where.not(id: id)
+    resources =
+      Resource.published.order('RANDOM()').limit(3).left_outer_joins(:series, :authors, :topics).where.not(id: id)
     resources = resources.where(series: { id: resource.series.pluck(:id) })
     resources = resources.or(resources.where(category_topics: { id: resource.topics.pluck(:id) }))
     resources = resources.or(resources.where(authors: { id: resource.authors.pluck(:id) }))
     resources.decorate
+  end
+
+  def description
+    return snippet if snippet.present?
+
+    author_names = authors.map(&:name).join(', ')
+    scripture_names = connection_scriptures.map(&:name).join(', ')
+    display_published_at = published_at.strftime('%b %d, %Y')
+
+    [author_names, scripture_names, display_published_at].select(&:present?).join(' | ')
   end
 end
