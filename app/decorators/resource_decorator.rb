af1ed9961206f069db0855fa8e1b52568ee9e3dd
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-05-28 22:35:16 +1200

    add sharing for resource

--- app/decorators/resource_decorator.rb
+++ app/decorators/resource_decorator.rb
@@ -14,4 +14,12 @@ class ResourceDecorator < ApplicationDecorator
   def type_param
     type.demodulize.downcase
   end
+
+  def related
+    resources = Resource.published.order('RANDOM()').limit(3).left_outer_joins(:series, :authors, :topics).where.not(id: id)
+    resources = resources.where(series: { id: resource.series.pluck(:id) })
+    resources = resources.or(resources.where(category_topics: { id: resource.topics.pluck(:id) }))
+    resources = resources.or(resources.where(authors: { id: resource.authors.pluck(:id) }))
+    resources.decorate
+  end
 end
