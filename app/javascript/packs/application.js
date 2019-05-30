4066853880eada3c2f45b2101f30b6d2b3919ac8
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-05-31 08:17:57 +1200

    remove history changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,14 +26,6 @@ $(document).on('turbolinks:load', () => {
     });
   });
 });
-$(document).on('turbolinks:visit', () => {
-  if (typeof dataLayer !== 'undefined') {
-    dataLayer.push({
-      'event':'pageView',
-      'virtualUrl': window.location.href
-    });
-  }
-});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
