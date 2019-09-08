62dca741c3ef64215a314ac7cac87aa530c84504
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-09-09 08:25:10 +1200

    add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
     authentication: :login,
     enable_starttls_auto: true
   }
+  config.default_url_options = { protocol: 'https', host: 'resources.aucklandev.co.nz' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
