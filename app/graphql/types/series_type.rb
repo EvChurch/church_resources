74730c313189f66fe585dbccce88e2cd6f552ce7
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-09-09 15:04:11 +1200

    update series

--- app/graphql/types/series_type.rb
+++ app/graphql/types/series_type.rb
@@ -7,4 +7,30 @@ class Types::SeriesType < Types::BaseObject
   field :banner_url, String, null: true
   field :foreground_url, String, null: true
   field :background_url, String, null: true
+
+  def banner_url
+    banner && polymorphic_url(banner)
+  end
+
+  def foreground_url
+    foreground && polymorphic_url(foreground)
+  end
+
+  def background_url
+    background && polymorphic_url(background)
+  end
+
+  protected
+
+  def banner
+    object.banner.presence || object.series.first&.banner
+  end
+
+  def foreground
+    object.foreground.presence || object.series.first&.foreground
+  end
+
+  def background
+    object.background.presence || object.series.first&.background
+  end
 end
