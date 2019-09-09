6a47325ce2349d3d9f1f299f942152133bcf90f0
Tataihono Nikora <tataihono.nikora@gmail.com>
2019-09-09 15:57:26 +1200

    allow filtering by ids

--- app/graphql/queries/series_query.rb
+++ app/graphql/queries/series_query.rb
@@ -2,17 +2,25 @@
 
 class Queries::SeriesQuery < Queries::BaseQuery
   type Types::SeriesType.connection_type, null: false
-  argument :resource_type, String, required: false
+  argument :ids, [ID], required: false, default_value: nil
+  argument :resource_type, String, required: false, default_value: nil
 
-  def resolve(resource_type: nil)
-    scope(resource_type).all
+  def resolve(ids:, resource_type:)
+    scope(ids, resource_type).all
   end
 
   protected
 
-  def scope(resource_type)
-    return ::Series if resource_type.blank?
+  def scope(ids, resource_type)
+    scope = ::Series.joins(:resources).where.not(resources: { published_at: nil }).order('resources.published_at desc')
 
-    ::Series.joins(:resources).where(resources: { type: Resource::TYPES[resource_type.to_sym] }).distinct
+    scope = filter_by_ids(scope, ids)
+    scope.where(resources: { type: Resource::TYPES[resource_type.to_sym] }).distinct if resource_type.present?
+
+    scope
+  end
+
+  def filter_by_ids(scope, ids)
+    ids.present? ? scope.where(id: ids) : scope
   end
 end
