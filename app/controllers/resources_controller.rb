6e0c934f12eb52e28ab243306a2da6a15db1c852
Tataihono Nikora <tataihono.nikora@gmail.com>
2025-06-06 10:41:53 +1200

    Refactor RSS caching and fix RuboCop offenses

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -9,9 +9,21 @@ class ResourcesController < ApplicationController
         load_resources
       end
       format.rss do
-        load_all_resources # This will set @resources, which is then used by index.rss.builder
-        # The `decorates_assigned :resources` line will handle decoration.
-        # Rails will implicitly render app/views/resources/index.rss.builder
+        resource_scope = rss_resources_scope
+        latest_update = resource_scope.maximum(:updated_at)
+
+        cache_key_parts = %w[v1 rss resources]
+        cache_key_parts << params[:resource_type] if params[:resource_type].present?
+        cache_key_parts << latest_update.utc.to_fs(:number) if latest_update
+
+        response.headers['Content-Type'] = 'application/rss+xml; charset=utf-8'
+
+        cached_rss_content = Rails.cache.fetch(cache_key_parts.compact.join('/'), expires_in: 1.hour) do
+          @resources = resource_scope.includes(:authors, :connection_scriptures)
+          render_to_string template: 'resources/index', formats: [:rss]
+        end
+
+        render xml: cached_rss_content
       end
     end
   end
@@ -32,21 +44,19 @@ class ResourcesController < ApplicationController
     @resources = @resources.page params[:page]
   end
 
-  def load_all_resources
-    return @resources if @resources
-
-    @resources = scope.order(published_at: :desc).published.includes(:authors, :connection_scriptures)
-    if params[:resource_type].present?
-      @resources = @resources.where(type: Resource::TYPES[params[:resource_type].to_sym])
-    end
-    @resources
-  end
-
   def load_resource
     @resource ||= scope.friendly.find(params[:id])
   end
 
   def scope
     ::Resource
+  end
+
+  def rss_resources_scope
+    resources = scope.order(published_at: :desc).published
+    if params[:resource_type].present? && Resource::TYPES.key?(params[:resource_type].to_sym)
+      resources = resources.where(type: Resource::TYPES[params[:resource_type].to_sym])
+    end
+    resources
   end
 end
