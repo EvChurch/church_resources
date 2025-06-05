bcb28ea1b43bc40a46f946eeeb7bd0ee64c58adb
Tataihono Nikora <tataihono.nikora@gmail.com>
2025-06-06 01:46:09 +1200

    feat: add FontAwesome helper to ActiveAdmin configuration for enhanced icon support

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -8,6 +8,8 @@ ActiveAdmin.setup do |config|
   #
   config.site_title = 'Church Resources'
 
+  config.helper FontAwesome::Sass::Rails::ViewHelpers
+
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
