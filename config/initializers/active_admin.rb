148e8623d38c286fd6660b1506f387a648caf4b6
Tataihono Nikora <tataihono.nikora@gmail.com>
2025-06-06 01:47:09 +1200

    chore: update ActiveAdmin site title to reflect rebranding to Ev Church

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -6,7 +6,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = 'Church Resources'
+  config.site_title = 'Ev Church Resources'
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
