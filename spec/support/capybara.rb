4c424714b46423ef8b3070349af5fe7abfb2aabd
Tataihono Nikora <tataihono.nikora@gmail.com>
2026-02-07 15:09:57 +1300

    chore: Update GitHub Actions workflow to install Chromium and enhance Capybara driver options for CI

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,9 +4,16 @@ require 'capybara/rspec'
 require 'capybara/cuprite'
 
 Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(
-    app, window_size: [1400, 1400], js_errors: true, timeout: 30
-  )
+  options = {
+    window_size: [1400, 1400],
+    js_errors: true,
+    timeout: 30
+  }
+  if ENV['CI']
+    options[:process_timeout] = 30
+    options[:browser_options] = { 'no-sandbox': nil, 'disable-dev-shm-usage': nil }
+  end
+  Capybara::Cuprite::Driver.new(app, **options)
 end
 
 Capybara.default_driver = :rack_test
